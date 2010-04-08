fd15ab3c40557c5712ca42b1fea58929f5f85fa6
paul.voss@burnabit.com <paul.voss@burnabit.com@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-08 15:45:30 +0000

    - updated em_conf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/burnabitivwtags/trunk@32044 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "burnabitivwtags".
 #
-# Auto generated 31-03-2010 10:44
+# Auto generated 08-04-2010 13:34
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"0661";s:7:"GPL.txt";s:4:"4fe8";s:19:"burnabitivwtags.php";s:4:"f9c9";s:21:"ext_conf_template.txt";s:4:"dd57";s:12:"ext_icon.gif";s:4:"a9d3";s:14:"ext_tables.php";s:4:"5e25";s:14:"ext_tables.sql";s:4:"eb24";s:24:"ext_typoscript_setup.txt";s:4:"ce56";s:16:"locallang_db.xml";s:4:"b047";s:7:"tca.php";s:4:"d41d";s:14:"doc/manual.sxw";s:4:"79d4";s:19:"res/ivwtag.1.5.tmpl";s:4:"aff2";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"0661";s:7:"GPL.txt";s:4:"4fe8";s:19:"burnabitivwtags.php";s:4:"f920";s:28:"class.tx_burnabitivwtags.php";s:4:"635d";s:21:"ext_conf_template.txt";s:4:"f088";s:12:"ext_icon.gif";s:4:"a9d3";s:14:"ext_tables.php";s:4:"1781";s:14:"ext_tables.sql";s:4:"6737";s:24:"ext_typoscript_setup.txt";s:4:"1dc5";s:16:"locallang_db.xml";s:4:"ac9b";s:7:"tca.php";s:4:"d41d";s:14:"doc/manual.sxw";s:4:"79d4";s:16:"flexform/ivw.xml";s:4:"7eb2";s:19:"res/ivwtag.1.5.tmpl";s:4:"aff2";}',
 );
 
 ?>
